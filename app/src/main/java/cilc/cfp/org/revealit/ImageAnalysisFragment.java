ffcd3c513b4d07c1e443984a8c471c3a9aec67e4
codernaut <gul.zaib@gmail.com>
2018-08-05 17:24:55 +0500

    initial commit

---
+++ app/src/main/java/cilc/cfp/org/revealit/ImageAnalysisFragment.java
@@ -0,0 +1,205 @@
+package cilc.cfp.org.revealit;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ImageAnalysisFragment.OnImageFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ImageAnalysisFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ImageAnalysisFragment extends Fragment implements View.OnClickListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    // Code for our image picker select action.
+    private static final int IMAGE_PICKER_SELECT = 999;
+
+    // Reference to our image view we will use
+    private ImageView mSelectedImage;
+
+    // Reference to picker button.
+    private Button mPickPhotoButton;
+
+    private Button mAnalyzeButton;
+
+    private OnImageFragmentInteractionListener mListener;
+
+    public ImageAnalysisFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ImageAnalysisFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ImageAnalysisFragment newInstance(String param1, String param2) {
+        ImageAnalysisFragment fragment = new ImageAnalysisFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_image_analysis, container, false);
+
+        // Set the image view
+        mSelectedImage = (ImageView)view.findViewById(R.id.imageViewFullSized);
+        mPickPhotoButton = (Button)view.findViewById(R.id.pickbutton);
+        mAnalyzeButton = (Button)view.findViewById(R.id.selectbutton);
+        // Set OnItemClickListener so we can be notified on button clicks
+
+        mPickPhotoButton.setOnClickListener(this);
+        mAnalyzeButton.setOnClickListener(this);
+
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onImageFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnImageFragmentInteractionListener) {
+            mListener = (OnImageFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, IMAGE_PICKER_SELECT);
+
+    }
+
+    /**
+     * Photo Selection result
+     */
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == IMAGE_PICKER_SELECT  && resultCode == Activity.RESULT_OK) {
+            MainLauncher activity = (MainLauncher)getActivity();
+            Bitmap bitmap = getBitmapFromCameraData(data, activity);
+            mSelectedImage.setImageBitmap(bitmap);
+        }
+    }
+
+    /**
+     * Scale the photo down and fit it to our image views.
+     *
+     * "Drastically increases performance" to set images using this technique.
+     * Read more:http://developer.android.com/training/camera/photobasics.html
+     */
+    private void setFullImageFromFilePath(String imagePath) {
+        // Get the dimensions of the View
+        int targetW = mSelectedImage.getWidth();
+        int targetH = mSelectedImage.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imagePath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath, bmOptions);
+        mSelectedImage.setImageBitmap(bitmap);
+    }
+
+    /**
+     * Use for decoding camera response data.
+     *
+     * @param data
+     * @param context
+     * @return
+     */
+    public static Bitmap getBitmapFromCameraData(Intent data, Context context){
+        Uri selectedImage = data.getData();
+        String[] filePathColumn = { MediaStore.Images.Media.DATA };
+        Cursor cursor = context.getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+        cursor.moveToFirst();
+        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+        String picturePath = cursor.getString(columnIndex);
+        cursor.close();
+        return BitmapFactory.decodeFile(picturePath);
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnImageFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onImageFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/cilc/cfp/org/revealit/MainLauncher.java
@@ -0,0 +1,120 @@
+package cilc.cfp.org.revealit;
+
+import android.app.FragmentTransaction;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainLauncher extends AppCompatActivity implements HomeFragment.OnHomeFragmentInteractionListener,ImageAnalysisFragment.OnImageFragmentInteractionListener,TextAnalysisFragment.OnTextFragmentInteractionListener {
+
+
+    HomeFragment homeFragment = new HomeFragment();
+    TextAnalysisFragment textAnalysisFragment = new TextAnalysisFragment();
+    ImageAnalysisFragment imageAnalysisFragment = new ImageAnalysisFragment();
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            android.support.v4.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                    Bundle args = new Bundle();
+                    homeFragment.setArguments(args);
+
+
+// Replace whatever is in the fragment_container view with this fragment,
+// and add the transaction to the back stack so the user can navigate back
+                    transaction.replace(R.id.fragment_container, homeFragment);
+                    transaction.addToBackStack(null);
+
+// Commit the transaction
+                    transaction.commit();
+
+                    return true;
+                case R.id.navigation_dashboard:
+                    //mTextMessage.setText(R.string.title_dashboard);
+
+                    Bundle args1 = new Bundle();
+                    textAnalysisFragment.setArguments(args1);
+
+
+// Replace whatever is in the fragment_container view with this fragment,
+// and add the transaction to the back stack so the user can navigate back
+                    transaction.replace(R.id.fragment_container, imageAnalysisFragment);
+                    transaction.addToBackStack(null);
+
+// Commit the transaction
+                    transaction.commit();
+                    return true;
+                case R.id.navigation_notifications:
+
+                    Bundle args2 = new Bundle();
+                    imageAnalysisFragment.setArguments(args2);
+
+
+// Replace whatever is in the fragment_container view with this fragment,
+// and add the transaction to the back stack so the user can navigate back
+                    transaction.replace(R.id.fragment_container, textAnalysisFragment);
+                    transaction.addToBackStack(null);
+
+// Commit the transaction
+                    transaction.commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_luncher);
+
+       /* mTextMessage = (TextView) findViewById(R.id.message);
+        mTextMessage.setText(R.string.app_desc);*/
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            homeFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, homeFragment).commit();
+        }
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public void onHomeFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onImageFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onTextFragmentInteraction(Uri uri) {
+
+    }
+}
